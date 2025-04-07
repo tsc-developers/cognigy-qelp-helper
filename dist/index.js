fd89eaf97bfefb8492be74f9c7e530b8fc3616cd
Sergio Marreiro <sergiocmarreiro@gmail.com>
2025-04-07 13:32:23 -0300

    fixing react reference/ compiling with targets

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,25 @@
-var $yx6n1$react = require("react");
+(() => {
+var $da2c81592e27e985$exports = {};
+var $d170ef2c6ed47533$exports = {};
+console.log('[React Alias] Using Cognigy Webchat React');
+if (!window.__COGNIGY_WEBCHAT) throw new Error('Cognigy Webchat v2.7 or higher has to be loaded before this plugin');
+$d170ef2c6ed47533$exports = window.__COGNIGY_WEBCHAT.React;
 
 
-const $91fc7c7f5ab63f5b$var$RedText = (props)=>{
+const $da2c81592e27e985$var$RedText = (props)=>{
     const text = props.message.text;
-    return /*#__PURE__*/ $yx6n1$react.createElement("span", {
+    return /*#__PURE__*/ $d170ef2c6ed47533$exports.createElement("span", {
         style: {
             color: 'red'
         }
     }, text);
 };
-const $91fc7c7f5ab63f5b$var$redTextPlugin = {
+const $da2c81592e27e985$var$redTextPlugin = {
     match: 'red-text',
-    component: $91fc7c7f5ab63f5b$var$RedText
+    component: $da2c81592e27e985$var$RedText
 };
 // create if it does not exist yet
 if (!window.cognigyWebchatMessagePlugins) window.cognigyWebchatMessagePlugins = [];
-window.cognigyWebchatMessagePlugins.push($91fc7c7f5ab63f5b$var$redTextPlugin);
+window.cognigyWebchatMessagePlugins.push($da2c81592e27e985$var$redTextPlugin);
 
-
-//# sourceMappingURL=index.js.map
+})();
